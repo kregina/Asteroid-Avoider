dc5a8aee4135501a8e6864abd901385e7c37edd0
Kelly Silva <kellynha756@gmail.com>
2023-07-08 18:10:30 +0300

    add gameover UI, play again and return to menu logic

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -2,8 +2,11 @@ using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
+    [SerializeField] private GameOverHandler gameOver;
     public void Crash()
     {
         gameObject.SetActive(false);
+        gameOver.EndGame();
+
     }
 }
