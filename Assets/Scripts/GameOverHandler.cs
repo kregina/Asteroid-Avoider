a545a2d52cd32372cbace5a26a501c0804735253
Kelly Silva <kellynha756@gmail.com>
2023-07-10 13:56:03 +0300

    add ad manager

--- Assets/Scripts/GameOverHandler.cs
+++ Assets/Scripts/GameOverHandler.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameOverHandler : MonoBehaviour
 {
@@ -10,6 +11,8 @@ public class GameOverHandler : MonoBehaviour
     [SerializeField] private GameObject gameOverDisplay;
     [SerializeField] private ScoreSystem scoreSystem;
     [SerializeField] private AsteroidSpawner asteroidSpawner;
+    [SerializeField] private GameObject player;
+    [SerializeField] private Button continueButton; 
 
     public void EndGame()
     {
@@ -22,6 +25,22 @@ public class GameOverHandler : MonoBehaviour
     public void PlayAgain()
     {
         SceneManager.LoadScene(1);
+    }
+
+    public void ContinueButon()
+    {
+        AdManager.Instance.ShowAd(this);
+        continueButton.interactable = false;
+    }
+
+    public void ContinueGame()
+    {
+        scoreSystem.StartTimer();
+        player.transform.position = Vector3.zero;
+        player.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        player.SetActive(true);
+        asteroidSpawner.enabled = true;
+        gameOverDisplay.gameObject.SetActive(false);
     }
 
     public void ReturnToMenu()
